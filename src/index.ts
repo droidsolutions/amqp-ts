4473bb4cb4abb5cccac29e540fc6a076fef353cb
Stefan Ißmer <stefan.issmer@droidsolutions.de>
2020-04-15 10:27:09 +0200

    chore: remove deleted interface from index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,12 @@ import { Topology } from "./Connection/Topology";
 import { DeclarationOptions as ExchangeDeclarationOptions } from "./Exchange/DeclarationOptions";
 import { Exchange } from "./Exchange/Exchange";
 import { Message } from "./Message";
-import { ActivateConsumerOptions } from "./Queue/ActivateConsumerOptions";
 import { DeclarationOptions as QueueDeclarationOptions } from "./Queue/DeclarationOptions";
 import { Queue } from "./Queue/Queue";
 import { StartConsumerOptions } from "./Queue/StartConsumerOptions";
 import { LoggerFactory } from "./LoggerFactory";
 
 export {
-  ActivateConsumerOptions,
   Binding,
   Connection,
   Exchange,
