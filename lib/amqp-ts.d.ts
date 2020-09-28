27c561917999edddc8cbab80e0a9216d11327d85
Stefan Ißmer <stefan.issmer@droidsolutions.de>
2020-09-28 16:27:31 +0200

    feat: emit jsdoc in transpiled js

--- lib/amqp-ts.d.ts
+++ lib/amqp-ts.d.ts
@@ -1,2 +1,8 @@
+/**
+ * Created by Ab on 17-9-2015.
+ *
+ * methods and properties starting with '_' signify that the scope of the item should be limited to
+ * the inside of the enclosing namespace.
+ */
 export declare const ApplicationName: string;
 export declare const DIRECT_REPLY_TO_QUEUE = "amq.rabbitmq.reply-to";
