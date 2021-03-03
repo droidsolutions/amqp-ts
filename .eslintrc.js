8100ae11ca9bec9b48e1241af89587783cc147ec
Stefan Ißmer <stefan.issmer@droidsolutions.de>
2021-03-03 15:06:57 +0100

    chore: fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "prettier/@typescript-eslint",
+    "prettier",
     "plugin:node/recommended-module",
   ],
   ignorePatterns: ["coverage", "lib", "test/__test", ".eslintrc.js"],
