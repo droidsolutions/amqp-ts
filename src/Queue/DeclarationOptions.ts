1c14cff6a8b4e8147f3d665eb595bca26b7e1df6
Stefan Ißmer <stefan.issmer@droidsolutions.de>
2021-03-09 13:09:55 +0100

    docs: add more JSDOC comments

--- src/Queue/DeclarationOptions.ts
+++ src/Queue/DeclarationOptions.ts
@@ -1,3 +1,4 @@
+/** Options for declaring queues. */
 export interface DeclarationOptions {
   exclusive?: boolean;
   durable?: boolean;
@@ -7,6 +8,11 @@ export interface DeclarationOptions {
   expires?: number;
   deadLetterExchange?: string;
   maxLength?: number;
+  /** Tells the server to not give more than this amount of messages at the same time to this queue. */
   prefetch?: number;
+  /**
+   * If true, checkQueue is called instead of assertQueue. See
+   * {@link http://www.squaremobius.net/amqp.node/channel_api.html#channel_checkQueue} for more info.
+   */
   noCreate?: boolean;
 }
