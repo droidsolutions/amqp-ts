b7dc0542d460dd883edf63a78d961027decf3558
Stefan Ißmer <stefan.issmer@droidsolutions.de>
2021-03-09 13:14:26 +0100

    refactor: use self provided AmqpProperties in Message
    Replaces the use of the original AmqpLib.Options.Publish in Message with the self provided
    AmqpProperties interface. This is to prevent users from having to install @types/amqplib to see
    what can be set in Message properties.

--- src/AmqpProperties.ts
+++ src/AmqpProperties.ts
@@ -1,10 +1,12 @@
+import * as AmqpLib from "amqplib";
+
 /**
  * Represents a set of optional properties that can be set when publishing messages via AMQP.
- * 
+ *
  * @see https://www.rabbitmq.com/consumers.html#message-properties for more info.
  * Also @see http://www.squaremobius.net/amqp.node/channel_api.html#channel_publish for library defaults.
  */
-export interface AmqpProperties {
+export interface AmqpProperties extends AmqpLib.Options.Publish {
   /** The time to live until the message is considered dead in ms. @see https://www.rabbitmq.com/ttl.html */
   expiration?: string | number;
   /**
@@ -12,7 +14,7 @@ export interface AmqpProperties {
    * of the user used to open the connection. @see https://www.rabbitmq.com/validated-user-id.html
    */
   userId?: string;
-  
+
   /**
    * An array of routing keys as string. Messages will be routed to these routing keys in addition to the given routing
    * key parameter.
@@ -23,18 +25,18 @@ export interface AmqpProperties {
   mandatory?: boolean;
   /**
    * Sets the @see deliveryMode to 2. If true the message will survive broker restarts as long as the queue survives.
-   * 
+   *
    * @example true
    */
   persistent?: boolean;
   /**
    * The delivery mode, 2 or true means "persistent", 1 or false "transient". Used by RabbitMQ but not sent to
    * consumers.
-   * 
+   *
    * @example 2
    */
   deliveryMode?: boolean | number;
-  
+
   /**
    * Like @see CC except that the headers will not be send in the message headers to the consumer.
    */
@@ -42,51 +44,51 @@ export interface AmqpProperties {
 
   /**
    * The content type of the message body, useful for consumer when parsing the message body.
-   * 
+   *
    * @example "application/json"
    */
   contentType?: string;
-  
+
   /**
    * The encoding of the message body, useful for consumer when parsing the body.
-   * 
+   *
    * @example "utf-8"
    */
   contentEncoding?: string;
-  
+
   /**
    * Any additional headers of the message. For content-type use @see contentType . Application specific and not used
    * by RabbitMQ.*/
   headers?: any;
-  
+
   /**
    * A positive integer that sets the priority of the message. @see http://www.rabbitmq.com/priority.html
    */
   priority?: number;
-  
+
   /** Useful to combine logs of sender and recipient by using a correlation ID. */
   correlationId?: string;
-  
+
   /** The name of the response queue. */
   replyTo?: string;
-  
+
   /** Arbitrary message ID. */
   messageId?: string;
-  
+
   /** A timestamp of the message. */
   timestamp?: number;
-  
+
   /**
    * Application specific message type. RabbitMQ does nothing with this field but it can help the consumer to know what
    * kind of message this is.
-   * 
+   *
    * @example "audit.log"
    */
   type?: string;
-  
+
   /**
    * The name of the application.
-   * 
+   *
    * @example "app:audit component:event-consumer"
    */
   appId?: string;
